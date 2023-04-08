4539eb0f075bd33d49466eea7c6167040c9f04ab
eyra-cc <88816593+eyra-cc@users.noreply.github.com>
2023-04-07 17:30:20 -0700

    Update index.m

--- content/publication/RQUM-2/index.m
+++ content/publication/RQUM-2/index.m
@@ -1,1 +1,41 @@
+title: Random Quasi-linear Utility
+subtitle: ''
+summary: ''
+authors:
+- Erya Yang
+tags: []
+categories: []
+date: '2023-03-28'
+lastmod: 2021-08-12T12:40:46-07:00
+featured: false
+draft: false
 
+# Featured image
+# To use, add an image named `featured.jpg/png` to your page's folder.
+# Focal points: Smart, Center, TopLeft, Top, TopRight, Left, Right, BottomLeft, Bottom, BottomRight.
+image:
+  caption: ''
+  focal_point: ''
+  preview_only: false
+
+# Projects (optional).
+#   Associate this post with one or more of your projects.
+#   Simply enter your project's folder or file name without extension.
+#   E.g. `projects = ["internal-project"]` references `content/project/deep-learning/index.md`.
+#   Otherwise, set `projects = []`.
+projects: []
+publishDate: '2021-08-12T19:40:46.118296Z'
+publication_types:
+- '2'
+abstract: ''
+publication: '*Jounral of Economic Theory*'
+
+url_pdf: https://drive.google.com/file/d/1s5Gapjm-Iwh6ubhRjZDqh0hpJQR4TiaX/view?usp=share_link
+doi: doi.org/10.1016/j.jet.2023.105650
+
+
+abstract: This paper incorporates fairness constraints into the classic single-unit reduced-form implementation problem (Border, 1991, 2007; Che, Kim & Mierendorff, 2013; Manelli & Vicent, 2010) with two agents. To do so, I use a new approach that utilizes the results from Kellerer (1961) and Gutmann, Kemperman, and Shepp (1991). Under realistic assumptions on the constraints, the conditions are transparent and can be verified in polynomial time.
+
+---
+
+{{< icon name="download" pack="fas" >}} Download {{< staticref "uploads/marginals.pdf" "newtab" >}}paper{{< /staticref >}}
